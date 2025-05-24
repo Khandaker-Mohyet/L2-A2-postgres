9fa2bae03e0b4666cd7e5cebde16b7511983813c
Khandaker-Mohyet <mohyet008@gmail.com>
2025-05-24 23:41:05 +0600

    create all table

--- index.sql
+++ index.sql
@@ -1,2 +1,26 @@
 -- Active: 1747418280166@@localhost@5432@conservation_db
 CREATE DATABASE conservation_db;
+
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    region TEXT
+);
+
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name TEXT NOT NULL,
+    scientific_name TEXT,
+    discovery_date DATE,
+    conservation_status TEXT
+);
+
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    FOREIGN KEY (ranger_id) REFERENCES rangers(ranger_id),
+    FOREIGN KEY (species_id) REFERENCES species(species_id),
+    sighting_time TIME,
+    location TEXT,
+    notes TEXT,
+);
+
